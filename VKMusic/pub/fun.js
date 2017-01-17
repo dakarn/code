e8a177ab2c3d2bf687bd7aba29cb861ad6a94bfc
Dakarn <dakarn9@mail.ru>
2017-01-17 08:49:38 +0400

    Signed-off-by: Dakarn <dakarn9@mail.ru>

---
+++ VKMusic/pub/fun.js
@@ -0,0 +1,59 @@
+var Music = (function( )
+{
+
+  window.addEventListener('load', function()
+  { 
+    document.getElementById('btn').addEventListener('click', function()
+    {
+      Music.requetsList();
+    });
+
+  });
+
+
+  function SendAjax( js )
+  {
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function()
+    { 
+       if ( req.readyState == 4 ){ js.even(req.responseText); }
+    }
+
+    req.open( 'POST' , js.path, true );
+    req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+    req.send( js.param );
+
+  }
+
+
+  return {
+
+
+   requetsList: function()
+   {
+
+     document.getElementById('content-answer').innerHTML = '<br><br><img src="pub/img/loader.gif"><br>\
+     Идет загрузка...';
+
+     SendAjax({ 
+     path: "pub/ajax.php",
+     param: "action=requetsList&title=",
+     even: function(data)
+     {
+            alert(data);
+     }});
+
+   },
+
+   
+   loadingFile: function()
+   {
+
+
+   }
+
+  };
+
+
+})();
